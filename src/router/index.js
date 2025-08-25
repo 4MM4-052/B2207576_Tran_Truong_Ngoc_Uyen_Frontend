0127e1e9f4222368a528a281d3c63ca2d8ad9531
4MM4-052 <cgtue0101004@gmail.com>
2025-08-25 11:35:22 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,23 @@ const routes = [
         name: "contactbook",
         component: ContactBook,
     },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notFound",
+        component: () => import ("@/views/NotFound.vue"),
+    }
+    // {
+    //     path: "/contacts/:id",
+    //     name: "contact.edit",
+    //     component: () => import ("@/views/ContactEdit.vue"),
+    //     props: true // truyền các biến trong $route.params vào làm props
+    // },
+    // {
+    //     path: "/contacts/add",
+    //     name: "contact.add",
+    //     component: () => import ("@/views/ContactAdd.vue"),
+    //     props: false
+    // }
 ];
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
