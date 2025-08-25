91fcee79f9eb1e5c230324f12c10280136f6bf4e
4MM4-052 <cgtue0101004@gmail.com>
2025-08-25 13:30:52 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -16,13 +16,13 @@ const routes = [
         name: "contact.edit",
         component: () => import ("@/views/ContactEdit.vue"),
         props: true // truyền các biến trong $route.params vào làm props
+    },
+    {
+        path: "/contacts/add",
+        name: "contact.add",
+        component: () => import ("@/views/ContactAdd.vue"),
+        props: false
     }
-    // {
-    //     path: "/contacts/add",
-    //     name: "contact.add",
-    //     component: () => import ("@/views/ContactAdd.vue"),
-    //     props: false
-    // }
 ];
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
